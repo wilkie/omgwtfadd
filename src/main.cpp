01916c14eec546a3ca72fd4346c7c8bd1581087b
wilkie <wilkie05@gmail.com>
2013-11-04 14:58:24 -0500

    Whitespace and cleanup of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,75 @@
-
 #include "engine.h"
 #include "main.h"
 
 #include "components.h"
 
 const char* strings[] = {
-	"ATTACK",
-	"TRANSITION",
-	"SUPER",
-	"TETRIS",
-	"YOU LOSE",
-	"YOU WIN",
-	"YOU SURVIVED",
+  "ATTACK",
+  "TRANSITION",
+  "SUPER",
+  "TETRIS",
+  "YOU LOSE",
+  "YOU WIN",
+  "YOU SURVIVED",
 };
 
-int main(int argc, char** argv)
-{
-	int port;
-	int isServer = 0;
-	char* ip=NULL;
+int main(int argc, char** argv) {
+  int port;
+  int isServer = 0;
+  char* ip=NULL;
 
-	SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Init(SDL_INIT_EVERYTHING);
 
-	if (argc > 1)
-	{
-		int i;
-		for (i=1; i<argc; i++)
-		{
-			if (strcmp(argv[i], "-p")==0)
-			{
-				i++;
-				if (i==argc) {break;}
+  if (argc > 1) {
+    int i;
+    for (i=1; i<argc; i++) {
+      if (strcmp(argv[i], "-p")==0) {
+        i++;
+        if (i==argc) {break;}
 
-				port = atoi(argv[i]);
-			}
-			else if (strcmp(argv[i], "-s") == 0)
-			{
-				printf("hosting...\n");
-				isServer = 1;
-			}
-			else
-			{
-				ip = argv[i];
-			}
-		}
+        port = atoi(argv[i]);
+      }
+      else if (strcmp(argv[i], "-s") == 0) {
+        printf("hosting...\n");
+        isServer = 1;
+      }
+      else {
+        ip = argv[i];
+      }
+    }
 
-		if (isServer && (ip != NULL))
-		{
-			// NO!
-			printf("Invalid Parameters, ignoring ip\n");
-		}
-		
-		if(SDLNet_Init()==-1) {
-			printf("SDLNet_Init: %s\n", SDLNet_GetError());
-			return -1;
-		}
+    if (isServer && (ip != NULL)) {
+      // NO!
+      printf("Invalid Parameters, ignoring ip\n");
+    }
 
-		if (isServer==0)
-		{
-			printf("connecting to port %d\n", port);
-			printf("connecting to %s\n", ip);
+    if(SDLNet_Init()==-1) {
+      printf("SDLNet_Init: %s\n", SDLNet_GetError());
+      return -1;
+    }
 
-			// Try to make a connection
-			engine.RunClient(ip, port);	
-		}
-		else
-		{
-			engine.RunServer(port);
-		}
-	}
+    if (isServer==0) {
+      printf("connecting to port %d\n", port);
+      printf("connecting to %s\n", ip);
 
-    // Create a double-buffered draw context
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);     
+      // Try to make a connection
+      engine.RunClient(ip, port);
+    }
+    else {
+      engine.RunServer(port);
+    }
+  }
 
-    SDL_SetVideoMode(800, 600, 0, SDL_OPENGL);// | SDL_FULLSCREEN);
+  // Create a double-buffered draw context
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-	engine.Init();
-    
-	engine.GameLoop();
+  SDL_SetVideoMode(800, 600, 0, SDL_OPENGL);// | SDL_FULLSCREEN);
 
-	SDL_Quit();
+  engine.Init();
 
-	return 0;
+  engine.GameLoop();
+
+  SDL_Quit();
+
+  return 0;
 }
